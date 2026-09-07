368aed61695df5a4e0970a1dd2477616cd0b9c24
Jarismic <jarismic@gmail.com>
2026-09-07 23:23:23 +1000

    Completed Brackeys tutorial E10
    + Added missile explosion effect
    + Added missile object
    + Added missile enemy tracking
    ~ Adjusted materials to reflect explosion lighting better

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
@@ -6,6 +7,7 @@ public class Bullet : MonoBehaviour
     public GameObject impactEffect;
 
     public float speed = 70f;
+    public float explosionRadius = 0f;
 
     private void Update()
     {
@@ -25,6 +27,7 @@ public class Bullet : MonoBehaviour
         }
 
         transform.Translate(direction.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
     }
 
     public void Seek(Transform _target)
@@ -36,8 +39,40 @@ public class Bullet : MonoBehaviour
     {
         // Spawn bullet destruction effect.
         GameObject effect = Instantiate(impactEffect, transform.position, transform.rotation);
-        Destroy(effect, 2f);
-        Destroy(target.gameObject);
+        Destroy(effect, 5f);
+
+        if (explosionRadius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
+        
         Destroy(gameObject);
+    }
+
+    private void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+
+    private void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.CompareTag("Enemy"))
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
